1d532846bc309bf8cd5bf5b60418ef21addb186f
Paulina7m <paulina7m@gmail.com>
2010-11-21 04:45:41 -0500

    All polygons for maps loaded, except ON and territories.

--- OmegaSquaddApplication/googlemap.cpp
+++ OmegaSquaddApplication/googlemap.cpp
@@ -4,15 +4,35 @@
       "<head>" \
         "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"/>" \
         "<title>The Canadian Epidemics Tracker Map</title>" \
-        "<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key=abcdefg&sensor=true_or_false\"" \
+        "<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=false&amp;key=ABQIAAAA19_FHK__lxCbVQEWBDCrZRS07ISVuDu6rafx1guXYWrGRDe03BRevP0Sf2SaJEOpZK-3RBCzzUxsyg\"" \
         "type=\"text/javascript\"></script>" \
         "<script type=\"text/javascript\">" \
         "function initialize() {" \
-          "if (GBrowserIsCompatible()) {" \
             "var map = new GMap2(document.getElementById(\"map_canvas\"));" \
-            "map.setCenter(new GLatLng(60.930432,-95.800781), 3);" \
+            "var geoXmlbritishcolumbia = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/britishcolumbia.kml\");" \
+            "var geoXmlalberta = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/alberta12.kml\");" \
+            "var geoXmlsaskatchewan = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/saskatchewan.kml\");" \
+            "var geoXmlmanitoba = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/manitoba.kml\");" \
+            "var geoXmlontariohealthunits = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/ontariohealthunits.kml\");" \
+            "var geoXmlquebec = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/quebec.kml\");" \
+            "var geoXmllabrador = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/labrador.kml\");" \
+            "var geoXmlprinceedward = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/princeedward.kml\");" \
+            "var geoXmlnovascotiazones = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/novascotiazones.kml\");" \
+            "var geoXmlnewbrunswick = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/newbrunswick.kml\");" \
+            "var geoXmlterritories = new GGeoXml(\"http://www.paulinamasson.com/attachments/kml/territories.kml\");" \
+            "map.setCenter(new GLatLng(61.438767,-103.183594), 3);" \
             "map.setUIToDefault();" \
-          "}" \
+            "map.addOverlay(geoXmlbritishcolumbia);" \
+            "map.addOverlay(geoXmlalberta);" \
+            "map.addOverlay(geoXmlsaskatchewan);" \
+            "map.addOverlay(geoXmlmanitoba);" \
+            "map.addOverlay(geoXmlontariohealthunits);" \
+            "map.addOverlay(geoXmlquebec);" \
+            "map.addOverlay(geoXmllabrador);" \
+            "map.addOverlay(geoXmlnovascotiazones);" \
+            "map.addOverlay(geoXmlprinceedward);" \
+            "map.addOverlay(geoXmlnewbrunswick);" \
+            "map.addOverlay(geoXmlterritories);" \
         "}" \
         "</script>" \
       "</head>" \
@@ -20,6 +40,10 @@
         "<div id=\"map_canvas\" style=\"width: 765px; height: 445px\"></div>" \
       "</body>" \
     "</html>"
+
+
+
+
 
     GoogleMap::GoogleMap(QWidget *parent): QWebView(parent){
         zoomPage = new QWebPage(this);
