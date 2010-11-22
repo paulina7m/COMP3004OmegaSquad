19d4cb2d8d0df67c388daa71388a9115b66b1233
spencerwinson <spencerwinson@hotmail.com>
2010-11-21 23:03:25 -0500

    Added functions

--- CETClientApplication/datahandler.cpp
+++ CETClientApplication/datahandler.cpp
@@ -50,12 +50,90 @@ QList<DiseaseType::DiseaseType> DataHandler::getDiseaseTypes()
 
 }
 
+//getProvinces
+//no parameters passed in because it doesn't make sense
+//of course it can be changed
+QList<Province::Province> DataHandler::getProvinces()
+{
+    QList<Province::Province> returnList;
+
+    //encode xml query
+
+    //send xml query to  communication component
+
+    //receive xml result from component
+
+    //dissect xml data and populate it into returnList
+
+
+    //return the list
+    return returnList;
+
+}
+
+//getRegions
+//no parameters passed in because it doesn't make sense
+//of course it can be changed
+QList<Region::Region> DataHandler::getRegions()
+{
+    QList<Region::Region> returnList;
+
+    //encode xml query
+
+    //send xml query to  communication component
+
+    //receive xml result from component
+
+    //dissect xml data and populate it into returnList
+
+
+    //return the list
+    return returnList;
+
+}
+
 //getinventory
 //follows stylings of get casereport
 QList<Inventory::Inventory> DataHandler::DataHandler(int idFrom, int idTo, int regionID, int supplyType, int quantityFrom, int quantityTo)
 {
     //create a list to be returned
     QList<Inventory::Inventory> returnList;
+
+    //encode parameters to xml query
+
+    //send xml query to  communication component
+
+    //receive xml result from component
+
+    //dissect xml data and populate it into returnList
+
+
+    //return the list
+    return returnList;
+}
+
+QList<Shipment::Shipment> DataHandler::getShipments(int idFrom, int idTo, int sendingRegion, int receivingRegion, int createdFrom, int createdTo, int shippedFrom, int shippedTo, int receivedFrom, int receivedTo, int cancelledFrom, int cancelledTo)
+{
+    //create a list to be returned
+    QList<Shipment::Shipment> returnList;
+
+    //encode parameters to xml query
+
+    //send xml query to  communication component
+
+    //receive xml result from component
+
+    //dissect xml data and populate it into returnList
+
+
+    //return the list
+    return returnList;
+}
+
+QList<ShipmentDetail::ShipmentDetail> DataHandler::get(int shipmentID)
+{
+    //create a list to be returned
+    QList<Shipment::Shipment> returnList;
 
     //encode parameters to xml query
 
